ba329f449059f5e8a787394967efbda7286e48c4
Naman <parmarnaman19@gmail.com>
2022-08-01 15:38:49 +0530

    firdt commit

---
+++ js/data.js
@@ -0,0 +1,18 @@
+const people = [
+	{
+		img: "https://lh3.googleusercontent.com/1QFYXScQeQAtCIsvtAEVMPNIJvkPjGvojQqD97_GQT1PV1LjX2KWzCI3D5LWu9pHwjAiyjmsA_wFdy45wrzdOFvR3bGSUU4OFND_3yCMOBYWXDlR3oQ7fw194dtnxFeFBE17gKm_pQ=w2400",
+		name: "Dharmik Dattani",
+		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+	},
+	{
+		img: "https://lh3.googleusercontent.com/zoBS7lihSzaLGHluwGDKfEyGBDY_phv5NVpQERWFExMX__yNmWZmoVli7blti4K_CLu5BdkWERYe8G0R065XXrCtSXXko_n3gPg8-o79nnvrXMfdtYTngu6o-geVEFlKBnV40IikOA=w2400",
+		name: "Naman Parmar",
+		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+	},
+	{
+		img: "https://lh3.googleusercontent.com/cYaUPioAOOVECqKuUmWQdSLDfAcezGEBygcpEX0Uty-5SSQypXyyx-JfPhT_V0mUxgMAuAdY1i5iuz1BSBYL14P6sW_YihESj-dJiGPLD_zRrOzEcJT6qZ3UYgqYT3FXW1GeAoR2Rg=w2400",
+		name: "Neal Darji",
+		text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+	},
+];
+export default people;
